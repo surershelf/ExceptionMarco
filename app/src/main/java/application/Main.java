db0c1d5c149a181cd81f07d69d0b03932dbc3e18
Lucas Mazzo <mazzolucas0@gmail.com>
2024-04-17 23:43:17 +0000

    17/04/24

--- app/src/main/java/application/Main.java
+++ app/src/main/java/application/Main.java
@@ -1,8 +1,28 @@
 package application;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+        int [] valores = new int [5];
+
+        for (int cont = 0;cont < valores.length;cont++){
+            try{
+                System.out.print("Digite um número inteiro:");
+                Scanner sc = new Scanner(System.in);
+                valores[cont] =sc.nextInt();
+            } catch(InputMismatchException ex){
+                System.out.println("Tipo Inválido");
+                cont--;
+            }
+            
+        }
+        System.out.println("Valores: ");
+        for (int val:valores){
+            System.out.println(val + " ");
+        
+        }
     }
 }
 
